48d4068d6b45fb0eedad7e381d7b54344247c3f3
Cristian Kubrak <kubrakcristian@gmail.com>
2018-09-10 17:03:56 -0300

    Experimentacion definitiva

--- expe/notebook.py
+++ expe/notebook.py
@@ -1,3 +1,4 @@
+
 #%%
 import random
 import os
@@ -6,8 +7,9 @@ import subprocess
 
 
 #%%
-for vDeseado in range(30,51,20):
-    for n in range (13,15):
+# TEST RANDOM
+for vDeseado in [x**2 for x in range(1,100)]:
+    for n in range (1,25):
         nombreArchivo = "/home/cristian/Facu/AlgoritmosIII/Tps/TP1/expe/randomV" + str(vDeseado) + "n" + str(n) +".txt"
         f = open(nombreArchivo, "a+")
         f.write(str(n)+" "+str(vDeseado)+ "\n")
@@ -15,11 +17,63 @@ for vDeseado in range(30,51,20):
             numero = random.randint(1,vDeseado + (vDeseado//10))
             f.write(str(numero)+ "\n")
         f.close() 
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 1 >> resultados/FBRandom.csv")
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 2 >> resultados/DinRandom.csv")
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 3 >> resultados/BackFactRandom.csv")
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 4 >> resultados/BackOptRandom.csv")
+
+        os.remove(nombreArchivo)
 
 
 #%%
-os.chdir("/home/cristian/Facu/AlgoritmosIII/Tps/TP1/expe/")
-for file in glob.glob("*.txt"):
-    os.system("cat " + str(file) + " | ../build/tp 1 >> resultados/FBRandom.csv")
-    os.system("cat " + str(file) + " | ../build/tp 2 >> resultados/DinRandom.csv")
-    os.system("cat " + str(file) + " | ../build/tp 3 >> resultados/BackFactRandom.csv")
+# TEST todos unos con vDeseado = n
+for n in range(1,25):
+        nombreArchivo = "/home/cristian/Facu/AlgoritmosIII/Tps/TP1/expe/unos_n" + str(n) +".txt"
+        f = open(nombreArchivo, "a+")
+        f.write(str(n)+" "+str(n)+ "\n")
+        for i in range (0,n):
+            numero = 1
+            f.write(str(numero)+ "\n")
+        f.close() 
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 1 >> resultados/FBunos.csv")
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 2 >> resultados/Dinunos.csv")
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 3 >> resultados/BackFactunos.csv")
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 4 >> resultados/BackOptunos.csv")
+
+        os.remove(nombreArchivo)
+
+#%%
+# TEST todos unos y no llega a vDeseado
+
+for vDeseado in [x**2 for x in range(1,100)]:
+        nombreArchivo = "/home/cristian/Facu/AlgoritmosIII/Tps/TP1/expe/unosNoLlega" + str(vDeseado) +".txt"
+        f = open(nombreArchivo, "a+")
+        f.write(str(n)+" "+str(vDeseado)+ "\n")
+        for n in range (0,25):
+            numero = 1
+            f.write(str(numero)+ "\n")
+        f.close() 
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 1 >> resultados/FBnoLlega.csv")
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 2 >> resultados/DinnoLlega.csv")
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 3 >> resultados/BackFactnoLlega.csv")
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 4 >> resultados/BackOptnoLlega.csv")
+
+        os.remove(nombreArchivo)
+
+
+#%%
+# TEST RANDOM EXIGENTE
+for vDeseado in [x**2 for x in range(1,100)]:
+    for n in range (1,150,3):
+        nombreArchivo = "/home/cristian/Facu/AlgoritmosIII/Tps/TP1/expe/randomV" + str(vDeseado) + "n" + str(n) +".txt"
+        f = open(nombreArchivo, "a+")
+        f.write(str(n)+" "+str(vDeseado)+ "\n")
+        for n in range (0,n):
+            numero = random.randint(1,vDeseado + (vDeseado//10))
+            f.write(str(numero)+ "\n")
+        f.close() 
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 2 >> resultados/DinExigenteRandom.csv")
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 3 >> resultados/BackExigenteFactRandom.csv")
+        os.system("cat " + str(nombreArchivo) + " | ../build/tp 4 >> resultados/BackOptExigenteRandom.csv")
+
+        os.remove(nombreArchivo)
