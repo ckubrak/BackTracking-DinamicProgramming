dfaebe7dea7ff7b8568d132c22dbae7b231b0658
Cristian Kubrak <kubrakcristian@gmail.com>
2018-09-16 03:09:07 -0300

    arreglo de prog dinamica con un poco de trampa

--- src/progDinamica.cpp
+++ src/progDinamica.cpp
@@ -1,3 +1,116 @@
+// #include <algorithm>
+// #include "progDinamica.h"
+
+
+// int resolverDinamica(int vDeseado, std::vector<int> &entrada, int i, int n, matriz &resultados, contador& count)
+// {
+//     count++;
+//     // if (vDeseado == 0)
+//     //     return 0;
+//     if (i == -1 )
+//     {
+//         return n + 1;
+//     }
+//     else
+//     {
+//         int resiV = resultados[i][vDeseado];
+//         if (resultados[i][vDeseado] == -2)
+//         {
+//             int entradaI = entrada[i];
+//             int parcial1 = resolverDinamica(vDeseado, entrada, i - 1, n, resultados, count);
+//             int parcial2 = 1 + resolverDinamica(vDeseado - entrada[i], entrada, i - 1, n, resultados, count);
+
+//             resultados[i][vDeseado] = std::min(parcial1, parcial2);
+//         }
+//         return resultados[i][vDeseado];
+//     }
+// }
+
+// int solucionDinamica (int vDeseado, std::vector<int>& entrada, contador& count)
+// {
+//     int res;
+//     int n = entrada.size();
+//     int i = n-1;
+//     std::vector<int> filaConBasura(vDeseado + 1,-2);
+//     matriz resultados(n,filaConBasura);
+
+//     for (int j = 0; j < resultados.size(); j++)
+//     {
+//         resultados[j][0] = 0;
+//     }
+
+//     res = resolverDinamica(vDeseado, entrada, i,n,resultados,count);
+//     if (res > entrada.size())
+//     {
+//         return -1;
+//     }
+    
+//     else
+//     {
+//         return res;
+//     }
+// }
+
+// #include <algorithm>
+// #include "progDinamica.h"
+
+
+// int resolverDinamica(int vDeseado, std::vector<int> &entrada, int i, int n, matriz &resultados, contador& count)
+// {
+//     // std::cout << vDeseado << " ";
+//     count++;
+//     if (i == -1 )
+//     {
+//         // std::cout << "i = -1" << std::endl;
+//         return n + 100;
+//     }
+//     if (vDeseado == 0)
+//     {
+//         // std::cout << "V == 0";
+//         return 0;
+//     }
+//     if (vDeseado < entrada[i])
+//         resultados[i][vDeseado] = resolverDinamica(vDeseado, entrada, i - 1, n, resultados, count);
+//         // return n+1;
+//     else
+//     {
+//         int resiV = resultados[i][vDeseado];
+//         // std::cout << " resIV " << resiV << std::endl;
+//         if (resultados[i][vDeseado] == -2)
+//         {
+//             int entradaI = entrada[i];
+//             int parcial1 = resolverDinamica(vDeseado, entrada, i - 1, n, resultados, count);
+//             std::cout << "Parcial 1 " << parcial1 << std::endl;
+//             int parcial2 = 1 + resolverDinamica(vDeseado - entrada[i], entrada, i - 1, n, resultados, count);
+//             std::cout << "Parcial 2 " << parcial2 << std::endl;
+//             std::cout << "resultados 2 2 " << resultados[2][2] << std::endl;
+//             std::cout << "parametros = " << (vDeseado - entrada[i]) << " " << (i-1) << std::endl;
+
+//             resultados[i][vDeseado] = std::min(parcial1, parcial2);
+//         }
+//         return resultados[i][vDeseado];
+//     }
+// }
+
+// int solucionDinamica (int vDeseado, std::vector<int>& entrada, contador& count)
+// {
+//     int res;
+//     int n = entrada.size();
+//     int i = n-1;
+//     std::vector<int> filaConBasura(vDeseado + 1,-2);
+//     matriz resultados(n,filaConBasura);
+
+//     res = resolverDinamica(vDeseado, entrada, i,n,resultados,count);
+//     if (res > entrada.size())
+//     {
+//         return -1;
+//     }
+    
+//     else
+//     {
+//         return res;
+//     }
+// }
 #include <algorithm>
 #include "progDinamica.h"
 
@@ -5,8 +118,10 @@
 int resolverDinamica(int vDeseado, std::vector<int> &entrada, int i, int n, matriz &resultados, contador& count)
 {
     count++;
-    // if (vDeseado == 0)
-    //     return 0;
+    if (vDeseado < 0)
+        return n+1;
+    if (vDeseado == 0)
+        return 0;
     if (i == -1 )
     {
         return n + 1;
@@ -17,10 +132,10 @@ int resolverDinamica(int vDeseado, std::vector<int> &entrada, int i, int n, matr
         if (resultados[i][vDeseado] == -2)
         {
             int entradaI = entrada[i];
-            int parcial1 = resolverDinamica(vDeseado, entrada, i - 1, n, resultados, count);
-            int parcial2 = 1 + resolverDinamica(vDeseado - entrada[i], entrada, i - 1, n, resultados, count);
+                int parcial1 = resolverDinamica(vDeseado, entrada, i - 1, n, resultados,count);
+                int parcial2 = 1 + resolverDinamica(vDeseado - entrada[i], entrada, i - 1, n, resultados,count);
 
-            resultados[i][vDeseado] = std::min(parcial1, parcial2);
+                resultados[i][vDeseado] = std::min(parcial1, parcial2);
         }
         return resultados[i][vDeseado];
     }
@@ -33,11 +148,6 @@ int solucionDinamica (int vDeseado, std::vector<int>& entrada, contador& count)
     int i = n-1;
     std::vector<int> filaConBasura(vDeseado + 1,-2);
     matriz resultados(n,filaConBasura);
-
-    for (int j = 0; j < resultados.size(); j++)
-    {
-        resultados[j][0] = 0;
-    }
 
     res = resolverDinamica(vDeseado, entrada, i,n,resultados,count);
     if (res > entrada.size())
