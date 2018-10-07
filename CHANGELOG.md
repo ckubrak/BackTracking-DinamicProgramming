acbf78ec6659840dc4756739bf65abc1f3efe036
Cristian Kubrak <kubrakcristian@gmail.com>
2018-10-07 00:28:39 -0300

    Update informe

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+Cambios en la experimentacion
+- Distribucion de los vi utilizando normalvariate
+- N mas grande para BackTracking y Dinamica
+
+Cambios en el informe
+- Cambios en experimentacion
+- Update de graficos
+- Correctitud y optimalidad
--- expe/notebook.py
+++ expe/notebook.py
@@ -2,33 +2,36 @@
 
 #%%
 import random
-import os
+import os 
 import glob
 import subprocess
+import numpy as np
 
 
 #%%
 # TEST RANDOM
-for x in range(0,10):
-    for vDeseado in [x**2 for x in range(1,80,3)]:
-        for n in range (1,30):
-            nombreArchivo = "/home/cristian/Facu/AlgoritmosIII/Tps/TP1/expe/randomV" + str(vDeseado) + "n" + str(n) +".txt"
+for x in range(0,3):
+    for vDeseado in [20,10000]:
+    # for vDeseado in [x**2 for x in range(6,200,3)]:
+        for n in range (1,1000,10):
+            nombreArchivo = "/home/cristian/Facu/AlgoritmosIII/Tps/TP1/expe/randomVV" + str(vDeseado) + "n" + str(n) +".txt"
             f = open(nombreArchivo, "a+")
             f.write(str(n)+" "+str(vDeseado)+ "\n")
+
             for n in range (0,n):
                 numero = random.randint(1,vDeseado - (vDeseado//10))
                 f.write(str(numero)+ "\n")
 
+
             f.close() 
-            os.system("cat " + str(nombreArchivo) + " | ../build/tp 1 >> resultados/FBRandom.csv")
-            os.system("cat " + str(nombreArchivo) + " | ../build/tp 2 >> resultados/DinRandomFinal.csv")
-            os.system("cat " + str(nombreArchivo) + " | ../build/tp 3 >> resultados/BackRandom.csv")
+            # os.system("cat " + str(nombreArchivo) + " | ../build/tp 1 >> nGrande/FBRandom.csv")
+            os.system("cat " + str(nombreArchivo) + " | ../build/tp 2 >> nGrande/DinRandomFinal.csv")
+            os.system("cat " + str(nombreArchivo) + " | ../build/tp 3 >> nGrande/BackRandom.csv")
 
             os.remove(nombreArchivo)
-
-    os.system("echo >> resultados/FBRandom.csv")
-    os.system("echo >> resultados/DinRandomFinal.csv")
-    os.system("echo >> resultados/BackRandom.csv")
+    # os.system("echo >> nGrande/FBRandom.csv")
+    os.system("echo >> nGrande/DinRandomFinal.csv")
+    os.system("echo >> nGrande/BackRandom.csv")
     print("Termino " + str(x))
 
 print("Terimno Random")
